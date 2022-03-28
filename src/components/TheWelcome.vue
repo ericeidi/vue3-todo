dce61eee3d5fff79fb6982b89724a7e991b87a0e
magdielmarques <contato.magdiel@gmail.com>
2022-03-28 10:46:49 -0400

    Adding new method - Magdiel

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-// import WelcomeItem from './WelcomeItem.vue'
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  methods: {
+    printSomething() {
+      console.log("Passei aqui");
+    },
+  },
+});
 </script>
 
 <template>
-  <h1> Todo List</h1>
+  <h1>Todo List</h1>
   <input type="text" placeholder="Task Name" />
-  <button type="submit">Add</button>
+  <button @click="printSomething" type="submit">Add</button>
 </template>
